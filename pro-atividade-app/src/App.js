20c249c6362fc5d7e928c2b52e1c14b98c549994
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2025-01-11 14:52:03 -0300

    Criando Components

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import AtividadeForm from './components/AtividadeForm';
 
 let initialState = [
   {
@@ -65,47 +66,7 @@ function App() {
 
   return (
     <>
-      <form className='row g-3'>
-        <div className="col-md-6">
-          <label for="id" className="form-label">Id</label>
-          <input 
-            type="text" 
-            className="form-control" 
-            readOnly
-            id="id" 
-            value={Math.max.apply(
-              Math, atividades.map(
-                atividade => atividade.id)) 
-              + 1}
-          />
-        </div>
-        <div className='col-md-6'>
-          <label className='form-label'>Prioridade</label>
-          <select id='prioridade' className='form-select'>
-            <option defaultValue='0'>Selecione..</option>
-            <option value='1'>Baixa</option>
-            <option value='2'>Normal</option>
-            <option value='3'>Alta</option>
-          </select>
-        </div>
-        <div className="col-md-6">
-          <label for="titulo" className="form-label">Título</label>
-          <input type="text" className="form-control" id="titulo" />
-        </div>
-        <div className="col-md-6">
-          <label for="descricao" className="form-label">Descrição</label>
-          <input type="text" className="form-control" id="descricao" />
-        </div>
-        <hr />
-        <div className='col-12'>
-          <button 
-            className='btn btn-outline-secondary' 
-            onClick={addAtividade}
-          >
-            + Atividade
-          </button>
-        </div>
-      </form>
+      <AtividadeForm />
       <div className="mt-3">
         { atividades.map((ativ) => (
           <div key={ativ.id} className={'card mb-2 shadow-sm border-' + prioridadeStyle(ativ.prioridade)}>
---
+++ pro-atividade-app/src/components/AtividadeForm.js
@@ -0,0 +1,45 @@
+export default function AtividadeForm() {
+    return (
+        <form className='row g-3'>
+        <div className="col-md-6">
+          <label for="id" className="form-label">Id</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            readOnly
+            id="id" 
+            value={Math.max.apply(
+              Math, atividades.map(
+                atividade => atividade.id)) 
+              + 1}
+          />
+        </div>
+        <div className='col-md-6'>
+          <label className='form-label'>Prioridade</label>
+          <select id='prioridade' className='form-select'>
+            <option defaultValue='0'>Selecione..</option>
+            <option value='1'>Baixa</option>
+            <option value='2'>Normal</option>
+            <option value='3'>Alta</option>
+          </select>
+        </div>
+        <div className="col-md-6">
+          <label for="titulo" className="form-label">Título</label>
+          <input type="text" className="form-control" id="titulo" />
+        </div>
+        <div className="col-md-6">
+          <label for="descricao" className="form-label">Descrição</label>
+          <input type="text" className="form-control" id="descricao" />
+        </div>
+        <hr />
+        <div className='col-12'>
+          <button 
+            className='btn btn-outline-secondary' 
+            onClick={addAtividade}
+          >
+            + Atividade
+          </button>
+        </div>
+      </form>
+    )
+}
