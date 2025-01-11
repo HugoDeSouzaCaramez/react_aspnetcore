0061ab195326f4f6de6e5a518cde8acdc6b3f6d6
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2025-01-11 17:59:15 -0300

    HTTP HTTPS

--- back/src/ProAtividade.API/Startup.cs
+++ back/src/ProAtividade.API/Startup.cs
@@ -44,7 +44,7 @@ namespace ProAtividade.API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProAtividade.API v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
