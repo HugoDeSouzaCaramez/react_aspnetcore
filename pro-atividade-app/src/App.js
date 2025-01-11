0b06161d41477e5e9ba6908527f500994f104b19
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2025-01-11 13:15:14 -0300

    Evoluindo Form

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 let initialState = [
   {
     id: 1,
+    prioridade: 'Baixa',
+    titulo: 'Título',
     descricao: 'Primeira Atividade',
   },
   {
     id: 2,
+    prioridade: 'Baixa',
+    titulo: 'Título',
     descricao: 'Segunda Atividade'
   },
 ];
@@ -20,6 +24,8 @@ function App() {
 
     const atividade = {
       id: document.getElementById('id').value,
+      prioridade: document.getElementById('prioridade').value,
+      titulo: document.getElementById('titulo').value,
       descricao: document.getElementById('descricao').value,
     };
 
@@ -32,6 +38,19 @@ function App() {
         <div className="col-md-6">
           <label for="id" className="form-label">Id</label>
           <input type="text" className="form-control" id="id" />
+        </div>
+        <div className='col-md-6'>
+          <label className='form-label'>Prioridade</label>
+          <select id='prioridade' className='form-select'>
+            <option defaultValue='0'>Selecione..</option>
+            <option value='1'>Baixa</option>
+            <option value='2'>Normal</option>
+            <option value='3'>Alta</option>
+          </select>
+        </div>
+        <div className="col-md-6">
+          <label for="titulo" className="form-label">Título</label>
+          <input type="text" className="form-control" id="titulo" />
         </div>
         <div className="col-md-6">
           <label for="descricao" className="form-label">Descrição</label>
@@ -54,13 +73,13 @@ function App() {
               <div className='d-flex justify-content-between'>
                 <h5 className='card-title'>
                   <span className='badge rounded-pil bg-secondary me-1'>{ativ.id}</span> 
-                   - título
+                   - {ativ.titulo}
                 </h5>
                 <h6>
                   Prioridade: 
                   <span className='ms-1 text-black'>
                     <i className='me-1 far fa-frown'></i>
-                    Alta
+                    {ativ.prioridade}
                   </span>
                 </h6>
               </div>
